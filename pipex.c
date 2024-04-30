7a8f44ae3fb02f1f787654794f31bb44b3f9c5a1
Nathaniel Jackson <njackson@student.42Adel.org.au>
2024-05-01 04:08:40 +0930

    Worked waitpid, fork, and execve

--- pipex.c
+++ pipex.c
@@ -6,20 +6,34 @@
 /*   By: njackson <njackson@student.42adel.o>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 14:37:27 by njackson          #+#    #+#             */
-/*   Updated: 2024/04/30 14:46:08 by njackson         ###   ########.fr       */
+/*   Updated: 2024/05/01 03:59:47 by njackson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.c"
+//#include "pipex.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "libft.h"
 
-int	main(int ac, char *av[])
+int	main(void)
 {
-	char	*out;
+	char *args[] = {"/bin/ls", 0};
+	char *env[] = {"PATH=/bin", 0};
+	pid_t	child;
+	int		status;
 
-	while (ac-- > 1)
+	child = fork();
+	if (child == 0)
 	{
-		out = run_command(av[ac]);
+		execve(args[0], args, env);
+		perror("ERROR: ");
+	}
+	else
+	{
+		ft_printf("<PARENT> Forked\n");
+		waitpid(-1, &status, 0);
 	}
 }
-
-
